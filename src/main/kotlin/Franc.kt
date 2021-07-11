e19baf35e8c90ff8560e72f7e7ef629863f5d1d0
tkhrttr <>
2021-07-11 13:09:10 +0900

    move equals from Frnac to Money

--- src/main/kotlin/Franc.kt
+++ src/main/kotlin/Franc.kt
@@ -1,13 +1,8 @@
-class Franc(private var amount: Int) {
+class Franc(amount: Int) : Money(amount) {
 
     // 型は引数のあとに定義
     fun times(multiplier: Int): Franc {
         return Franc(amount * multiplier)
-    }
-
-    override fun equals(any: Any?): Boolean {
-        val franc: Franc = any as Franc
-        return amount == franc.amount
     }
 
 }
